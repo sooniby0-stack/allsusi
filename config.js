250e79892f298a37875c0e20135d75b806900cc8
sooniby0-stack <sooniby0@cberi.go.kr>
2026-09-14 09:49:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Apps Script 배포 후 나오는 웹앱 URL을 아래에 붙여넣으세요.
 // 예: https://script.google.com/macros/s/AKfycbx.../exec
-const APPS_SCRIPT_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyPWSchiY7-4Y-G2oYO56g8KlTKCi31GjVXHChGyHtLzFOxpfI8D_D3b2trOll9EqKvCg/exec';
